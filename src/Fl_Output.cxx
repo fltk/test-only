0b44e6f268a5e352c6b110f0beef4984f58bce71
carl <carl.svn@fltk.example.org>
1999-11-10 15:45:38 +0000

    Removed redundancy.
    Carl
    
    
    git-svn-id: file:///fltk/svn/trunk@843 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Output.cxx
+++ src/Fl_Output.cxx
@@ -1,5 +1,5 @@
 //
-// "$Id: Fl_Output.cxx,v 1.19 1999/11/10 14:40:39 carl Exp $"
+// "$Id: Fl_Output.cxx,v 1.20 1999/11/10 15:45:38 carl Exp $"
 //
 // Output widget for the Fast Light Tool Kit (FLTK).
 //
@@ -48,11 +48,9 @@ int Fl_Output::handle(int event) {
 Fl_Output::Fl_Output(int x, int y, int w, int h, const char *l)
   : Fl_Input_(x, y, w, h, l)
 {
-  style(&Fl_Output::default_style); // so that Fl_Output's style will be a
-                                   // child of Fl_Input's style
   style(&default_style);
 }
 
 //
-// End of "$Id: Fl_Output.cxx,v 1.19 1999/11/10 14:40:39 carl Exp $".
+// End of "$Id: Fl_Output.cxx,v 1.20 1999/11/10 15:45:38 carl Exp $".
 //
