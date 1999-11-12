ad82db1da10929c0c4be37406e5bab612cdc4915
carl <carl.svn@fltk.example.org>
1999-11-12 13:48:18 +0000

    Switched which check button is originally on.
    Carl
    
    
    git-svn-id: file:///fltk/svn/trunk@860 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- test/pack.cxx
+++ test/pack.cxx
@@ -1,5 +1,5 @@
 //
-// "$Id: pack.cxx,v 1.5 1999/03/14 06:46:47 carl Exp $"
+// "$Id: pack.cxx,v 1.6 1999/11/12 13:48:18 carl Exp $"
 //
 // Fl_Pack test program for the Fast Light Tool Kit (FLTK).
 //
@@ -59,60 +59,60 @@ int main(int argc, char **argv) {
  {Fl_Window* o = new Fl_Window(365, 525);
   w = o;
   scroll = new Fl_Scroll(10,10,345,285);
- {Fl_Pack* o = new Fl_Pack(10, 10, 345, 285);
-  pack = o;
-  o->box(FL_DOWN_FRAME);
-  //o->box(FL_ENGRAVED_FRAME);
- (void) new Fl_Button(35, 35, 25, 25, "b1");
- (void) new Fl_Button(45, 45, 25, 25, "b2");
- (void) new Fl_Button(55, 55, 25, 25, "b3");
- (void) new Fl_Button(65, 65, 25, 25, "b4");
- (void) new Fl_Button(75, 75, 25, 25, "b5");
- (void) new Fl_Button(85, 85, 25, 25, "b6");
- (void) new Fl_Button(95, 95, 25, 25, "b7");
- (void) new Fl_Button(105, 105, 25, 25, "b8");
- (void) new Fl_Button(115, 115, 25, 25, "b9");
- (void) new Fl_Button(125, 125, 25, 25, "b10");
- (void) new Fl_Button(135, 135, 25, 25, "b11");
- (void) new Fl_Button(145, 145, 25, 25, "b12");
- (void) new Fl_Button(155, 155, 25, 25, "b13");
- (void) new Fl_Button(165, 165, 25, 25, "b14");
- (void) new Fl_Button(175, 175, 25, 25, "b15");
- (void) new Fl_Button(185, 185, 25, 25, "b16");
- (void) new Fl_Button(195, 195, 25, 25, "b17");
- (void) new Fl_Button(205, 205, 25, 25, "b18");
- (void) new Fl_Button(215, 215, 25, 25, "b19");
- (void) new Fl_Button(225, 225, 25, 25, "b20");
- (void) new Fl_Button(235, 235, 25, 25, "b21");
- (void) new Fl_Button(245, 245, 25, 25, "b22");
- (void) new Fl_Button(255, 255, 25, 25, "b23");
- (void) new Fl_Button(265, 265, 25, 25, "b24");
-  o->end();
-  w->resizable(o);
- }
- scroll->end();
- {Fl_Light_Button* o = new Fl_Light_Button(10, 325, 175, 25, "HORIZONTAL");
- o->type(FL_RADIO_BUTTON);
-  o->callback((Fl_Callback*)type_cb, (void*)(Fl_Pack::HORIZONTAL));
- }
- {Fl_Light_Button* o = new Fl_Light_Button(10, 350, 175, 25, "VERTICAL");
- o->type(FL_RADIO_BUTTON);
- o->value(1);
-  o->callback((Fl_Callback*)type_cb, (void*)(Fl_Pack::VERTICAL));
- }
- {Fl_Value_Slider* o = new Fl_Value_Slider(50,375, 295,25,"spacing:");
- o->align(FL_ALIGN_LEFT);
- o->type(FL_HORIZONTAL);
- o->range(0,30);
- o->step(1);
- o->callback((Fl_Callback*)spacing_cb);
- }
- w->end();
+  {Fl_Pack* o = new Fl_Pack(10, 10, 345, 285);
+   pack = o;
+   o->box(FL_DOWN_FRAME);
+   //o->box(FL_ENGRAVED_FRAME);
+   (void) new Fl_Button(35, 35, 25, 25, "b1");
+   (void) new Fl_Button(45, 45, 25, 25, "b2");
+   (void) new Fl_Button(55, 55, 25, 25, "b3");
+   (void) new Fl_Button(65, 65, 25, 25, "b4");
+   (void) new Fl_Button(75, 75, 25, 25, "b5");
+   (void) new Fl_Button(85, 85, 25, 25, "b6");
+   (void) new Fl_Button(95, 95, 25, 25, "b7");
+   (void) new Fl_Button(105, 105, 25, 25, "b8");
+   (void) new Fl_Button(115, 115, 25, 25, "b9");
+   (void) new Fl_Button(125, 125, 25, 25, "b10");
+   (void) new Fl_Button(135, 135, 25, 25, "b11");
+   (void) new Fl_Button(145, 145, 25, 25, "b12");
+   (void) new Fl_Button(155, 155, 25, 25, "b13");
+   (void) new Fl_Button(165, 165, 25, 25, "b14");
+   (void) new Fl_Button(175, 175, 25, 25, "b15");
+   (void) new Fl_Button(185, 185, 25, 25, "b16");
+   (void) new Fl_Button(195, 195, 25, 25, "b17");
+   (void) new Fl_Button(205, 205, 25, 25, "b18");
+   (void) new Fl_Button(215, 215, 25, 25, "b19");
+   (void) new Fl_Button(225, 225, 25, 25, "b20");
+   (void) new Fl_Button(235, 235, 25, 25, "b21");
+   (void) new Fl_Button(245, 245, 25, 25, "b22");
+   (void) new Fl_Button(255, 255, 25, 25, "b23");
+   (void) new Fl_Button(265, 265, 25, 25, "b24");
+   o->end();
+   w->resizable(o);
+  }
+  scroll->end();
+  {Fl_Light_Button* o = new Fl_Light_Button(10, 325, 175, 25, "HORIZONTAL");
+   o->type(FL_RADIO_BUTTON);
+   o->value(1);
+   o->callback((Fl_Callback*)type_cb, (void*)(Fl_Pack::HORIZONTAL));
+  }
+  {Fl_Light_Button* o = new Fl_Light_Button(10, 350, 175, 25, "VERTICAL");
+   o->type(FL_RADIO_BUTTON);
+   o->callback((Fl_Callback*)type_cb, (void*)(Fl_Pack::VERTICAL));
+  }
+  {Fl_Value_Slider* o = new Fl_Value_Slider(50,375, 295,25,"spacing:");
+   o->align(FL_ALIGN_LEFT);
+   o->type(FL_HORIZONTAL);
+   o->range(0,30);
+   o->step(1);
+   o->callback((Fl_Callback*)spacing_cb);
+  }
+  w->end();
  }
  w->show(argc, argv);
  return Fl::run();
 }
 
 //
-// End of "$Id: pack.cxx,v 1.5 1999/03/14 06:46:47 carl Exp $".
+// End of "$Id: pack.cxx,v 1.6 1999/11/12 13:48:18 carl Exp $".
 //
