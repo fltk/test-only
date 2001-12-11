69c4a11237072a46b1998a36e8475c2ebd1b2e57
spitzak <spitzak.svn@fltk.example.org>
2001-12-11 05:48:45 +0000

    Added missing files
    git-svn-id: file:///fltk/svn/trunk@1832 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

---
+++ fltk/Fl_Tiled_Image.h
@@ -0,0 +1,47 @@
+//
+// "$Id: Fl_Tiled_Image.h,v 1.1 2001/12/11 05:48:44 spitzak Exp $"
+//
+// Tiled image header file for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998-2001 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "fltk-bugs@fltk.org".
+//
+
+#ifndef Fl_Tiled_Image_h
+#define Fl_Tiled_Image_h
+
+#include "Fl_Image.h"
+
+// Tiled image class. 
+class Fl_Tiled_Image : public Fl_Image {
+protected:
+  Fl_Image* image_;
+public:
+  Fl_Tiled_Image(Fl_Image *i) : Fl_Image(), image_(i) {}
+  Fl_Image* image() const {return image_;}
+  void image(Fl_Image* i) {image_ = i;}
+  void measure(int& w, int& h);
+  void draw(int x, int y, int w, int h, Fl_Flags = 0);
+};
+
+#endif // !Fl_Tiled_Image_H
+
+//
+// End of "$Id: Fl_Tiled_Image.h,v 1.1 2001/12/11 05:48:44 spitzak Exp $"
+//
---
+++ src/Fl_Tiled_Image.cxx
@@ -0,0 +1,79 @@
+//
+// "$Id: Fl_Tiled_Image.cxx,v 1.2 2001/12/11 05:48:45 spitzak Exp $"
+//
+// Tiled image code for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998-2001 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "fltk-bugs@fltk.org".
+//
+
+#include <fltk/Fl_Tiled_Image.h>
+#include <fltk/fl_draw.h>
+
+// A tiled image "resizes" to completely fill any passed bounding box,
+// so this can just return the values unchanged. This maximizes the
+// values with the image size, though, because that allows the align
+// code to change the part of the image seen when the image is bigger
+// than the are to tile, and to fix old code that did not initialize
+// the w & h, or code that assummes this always returns values greater
+// than zero.
+void Fl_Tiled_Image::measure(int& w, int& h) {
+  int iw = w;
+  int ih = h;
+  image_->measure(iw,ih);
+  if (iw > w) w = iw;
+  if (ih > h) h = ih;
+}
+
+// Tiled image with minimal redraw
+void Fl_Tiled_Image::draw(int x, int y, int w, int h, Fl_Flags flags)
+{
+  int iw = w;
+  int ih = h;
+  image_->measure(iw,ih); if (iw <= 0 || ih <= 0) return;
+  int cx = 0;
+  int cy = 0;
+  // Perhaps this should use the Align flags to set cx, cy.
+
+  // Figure out the smallest rectangle enclosing this and the clip region:
+  int X,Y,W,H; fl_clip_box(x, y, w, h, X, Y, W, H);
+  if (W <= 0 || H <= 0) return;
+  cx += X-x; cy += Y-y;
+  fl_push_clip(X, Y, W, H);
+
+  int temp = -cx % iw;
+  cx = (temp>0 ? iw : 0) - temp;
+  temp = -cy % ih;
+  cy = (temp>0 ? ih : 0) - temp;
+
+  int ccx=cx;
+  while (-cy < H) {
+    while (-cx < W) {
+      image_->draw(X-cx, Y-cy, iw, ih, 0);
+      cx -= iw;
+    }
+    cy -= ih;
+    cx = ccx;
+  }
+  fl_pop_clip();
+}
+
+//
+// End of "$Id: Fl_Tiled_Image.cxx,v 1.2 2001/12/11 05:48:45 spitzak Exp $".
+//
