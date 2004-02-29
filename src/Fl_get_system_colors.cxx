713ddefb4f576246b3059d81bfbed812755326d6
easysw <easysw.svn@fltk.example.org>
2004-02-29 22:40:13 +0000

    Mirror 1.1.x changes.
    git-svn-id: file:///fltk/svn/branches/branch-1.2@3218 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_get_system_colors.cxx
+++ src/Fl_get_system_colors.cxx
@@ -1,5 +1,5 @@
 //
-// "$Id: Fl_get_system_colors.cxx,v 1.6.2.7.2.13.2.4 2003/12/02 02:51:47 easysw Exp $"
+// "$Id: Fl_get_system_colors.cxx,v 1.6.2.7.2.13.2.5 2004/02/29 22:40:13 easysw Exp $"
 //
 // System color support for the Fast Light Tool Kit (FLTK).
 //
@@ -70,7 +70,6 @@ void Fl::foreground(uchar r, uchar g, uchar b) {
 }
 
 void Fl::background2(uchar r, uchar g, uchar b) {
-  fl_fg_set  = 1;
   fl_bg2_set = 1;
 
   Fl::set_color(FL_BACKGROUND2_COLOR,r,g,b);
@@ -333,5 +332,5 @@ int Fl::reload_scheme() {
 
 
 //
-// End of "$Id: Fl_get_system_colors.cxx,v 1.6.2.7.2.13.2.4 2003/12/02 02:51:47 easysw Exp $".
+// End of "$Id: Fl_get_system_colors.cxx,v 1.6.2.7.2.13.2.5 2004/02/29 22:40:13 easysw Exp $".
 //
