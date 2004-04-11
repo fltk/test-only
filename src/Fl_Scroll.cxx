05be2c6f36e746204473e730df22928c8b360493
easysw <easysw.svn@fltk.example.org>
2004-04-11 01:40:59 +0000

    Fix fl_scroll() on WIN32 (STR #315)
    git-svn-id: file:///fltk/svn/branches/branch-1.2@3388 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Scroll.cxx
+++ src/Fl_Scroll.cxx
@@ -1,5 +1,5 @@
 //
-// "$Id: Fl_Scroll.cxx,v 1.7.2.6.2.2.2.7 2004/04/10 00:44:59 easysw Exp $"
+// "$Id: Fl_Scroll.cxx,v 1.7.2.6.2.2.2.8 2004/04/11 01:40:59 easysw Exp $"
 //
 // Scroll widget for the Fast Light Tool Kit (FLTK).
 //
@@ -74,7 +74,7 @@ void Fl_Scroll::draw_clip(void* v,int X, int Y, int W, int H) {
 	                       W+Fl::scheme_bg_->w(),
 			       H+Fl::scheme_bg_->h());
 	  break;
-        } else if (s->box() == FL_NO_BOX) break;
+        }
 
     default :
 	fl_color(s->color());
@@ -271,5 +271,5 @@ int Fl_Scroll::handle(int event) {
 }
 
 //
-// End of "$Id: Fl_Scroll.cxx,v 1.7.2.6.2.2.2.7 2004/04/10 00:44:59 easysw Exp $".
+// End of "$Id: Fl_Scroll.cxx,v 1.7.2.6.2.2.2.8 2004/04/11 01:40:59 easysw Exp $".
 //
