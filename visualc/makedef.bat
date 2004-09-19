6b338c3129e2f622d158c1fb87cd911218e3ade4
leka <leka.svn@fltk.example.org>
2004-09-19 18:40:58 +0000

    Since noone complained and reported problems in /ide/visualc I am removing /visualc from FLTK2 repository, as was planned.
    git-svn-id: file:///fltk/svn/trunk@3834 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- visualc/makedef.bat
+++
@@ -1,8 +0,0 @@
-echo EXPORTS > fltk.def
-nm ../lib/fltkd.lib | sed 's/?/_?/' > tmp
-sed 's/_WinMain//' < tmp > tmp2
-sed 's/_\$E//' < tmp2 > tmp
-sed 's/_??_G//' < tmp > tmp2
-grep '^........ [TRDB] _' | sed 's/[^_?]*[_?]//' >> fltk.def < tmp2
-del tmp
-del tmp2
