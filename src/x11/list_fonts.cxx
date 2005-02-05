b241b4836b9c9d255a12ae1f21220611df64c29a
mikko <mikko.svn@fltk.example.org>
2005-02-05 18:52:04 +0000

    Added needed file
    git-svn-id: file:///fltk/svn/trunk@4016 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

---
+++ src/x11/list_fonts.cxx
@@ -0,0 +1,30 @@
+//
+// "$Id: $"
+//
+// Copyright 1998-2000 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "fltk-bugs@fltk.org".
+//
+
+#if USE_XFT
+/* TODO: Separate list code from Font_xft.cxx to list_fonts_xft.cxx; 
+	 just to make code place consistent. */
+/*# include "list_fonts_xft.cxx"*/
+#else
+# include "list_fonts_xlfd.cxx"
+#endif
