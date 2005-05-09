1727e126b26532a7142028d3f60e4045690130cc
dejan <dejan.svn@fltk.example.org>
2005-05-09 22:35:13 +0000

    Fix of the fix. :)
    git-svn-id: file:///fltk/svn/trunk@4325 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- fluid/Fl_Type.cxx
+++ fluid/Fl_Type.cxx
@@ -70,7 +70,7 @@ const Enumeration* from_value(void* data, const Enumeration* table)
 const Enumeration* from_value(int data, const Enumeration* table)
 {
   for (;table->menu_entry; table++)
-    if (*(int*)(table->compiled) == data) return table;
+    if (*(int*)(&table->compiled) == data) return table;
   return 0;
 }
 
@@ -102,7 +102,7 @@ int number_from_text(const char* text, const Enumeration* table)
 {
   if (table) {
     const Enumeration* t = from_text(text, table);
-    if (t) return *(int*)(t->compiled);
+    if (t) return *(int*)(&t->compiled);
   }
   return strtol(text, 0, 0);
 }
