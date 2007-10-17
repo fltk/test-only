6b1bb15130a066b69172d18297fa791ff83a4d09
spitzak <spitzak.svn@fltk.example.org>
2007-10-17 20:54:58 +0000

    Newer VC++ was producing warnings about not putting underscore in front of Unix functions.
    git-svn-id: file:///fltk/svn/trunk@5961 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- fltk/string.h
+++ fltk/string.h
@@ -47,8 +47,8 @@
 /* Windows has equivalent functions, but being Microsoft they added
    gratuitoius changes to the names to stop code from being portable: */
 #if (defined(_WIN32) && !defined(__CYGWIN__)) || defined(__EMX__)
-# define strcasecmp(s,t)	stricmp(s, t)
-# define strncasecmp(s,t,n)	strnicmp(s, t, n)
+# define strcasecmp(s,t)	_stricmp(s, t)
+# define strncasecmp(s,t,n)	_strnicmp(s, t, n)
 # define vsnprintf		_vsnprintf
 # define snprintf		_snprintf
 #endif
