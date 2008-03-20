d94825cdb4f6e46c6f50c2d2822d11598e0265af
fabien <fabien.svn@fltk.example.org>
2008-03-20 17:11:58 +0000

    fixed STR#1878: FLUID2 consuming 100% of CPU time
    git-svn-id: file:///fltk/svn/trunk@6075 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- fluid/FunctionType.cxx
+++ fluid/FunctionType.cxx
@@ -741,13 +741,9 @@ void CommentType::open() {
     comment_in_source->value(in_c_ ? true : false);
     comment_in_header->value(in_h_ ? true : false);
     comment_panel_ok->callback(comment_ok_cb);
-    comment_panel->show();
     char itempath[256]; itempath[0] = 0;
-
-    do { // read events
-	fltk::check();
-    } while (comment_panel->shown());
-
+    comment_panel->exec(0,false);
+ 
     title_buf[0] = 0;
     comment_panel->hide();
 #endif
