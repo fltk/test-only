803be508808044ba833978b9ba9a79d2483b220d
spitzak <spitzak.svn@fltk.example.org>
2008-10-05 22:23:51 +0000

    Fix for STR # 2050 Obey the RAW_LABEL (and other) flags in tab label
    git-svn-id: file:///fltk/svn/trunk@6382 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/TabGroup.cxx
+++ src/TabGroup.cxx
@@ -471,7 +471,7 @@ void TabGroup::draw_tab(int x1, int x2, int W, int H, Widget* o, int what) {
 		abs(H)-1);
     drawstyle(o->style(), sel && focused() ? FOCUSED|OUTPUT : OUTPUT);
     setcolor(sel  ? selection_textcolor() : o->textcolor());
-    o->draw_label(r, ALIGN_CENTER);
+    o->draw_label(r, (o->flags() & ~ALIGN_MASK)|ALIGN_CENTER);
     FLAT_BOX->draw_symbol_overlay(r);
   }
 }
