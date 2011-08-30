36f248cf4e5b6a52a17c4aeffe4d46ee4e6bce3f
AlbrechtS <AlbrechtS.svn@fltk.example.org>
2011-08-30 08:01:17 +0000

    Fixed fl_pie() drawing too small on X11 (STR #2703)
    git-svn-id: file:///fltk/svn/branches/branch-3.0@9024 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/fltk3/arci.cxx
+++ src/fltk3/arci.cxx
@@ -86,6 +86,7 @@ void fltk3::GraphicsDriver::pie(int x,int y,int w,int h,double a1,double a2) {
   if (w <= 0 || h <= 0) return;
 
 #if defined(USE_X11)
+  XDrawArc(fl_display, fl_window, fl_gc, x,y,w-1,h-1, int(a1*64),int((a2-a1)*64));
   XFillArc(fl_display, fl_window, fl_gc, x,y,w-1,h-1, int(a1*64),int((a2-a1)*64));
 #elif defined(WIN32)
   if (a1 == a2) return;
